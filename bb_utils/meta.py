302cda490cf1e177bd0f8a6ddc97dd70ffa6944e
Benjamin Wild <b.w@fu-berlin.de>
2017-05-24 15:57:23 +0200

    Check date in BeeMetaInfo

--- bb_utils/meta.py
+++ bb_utils/meta.py
@@ -11,6 +11,10 @@ class BeeMetaInfo:
         self.hatchdates = pd.read_csv(hatchdates_path)
         self.hatchdates.hatchdate = pd.to_datetime(self.hatchdates.hatchdate, format='%d.%m.%Y')
 
+    def _check_date(self, timestamp):
+        if timestamp.year != 2016:
+            raise ValueError('Meta information only available for season 2016')
+
     def get_hatchdate(self, bee_id):
         """Get hatchdate of the bee with the given ID.
 
@@ -36,6 +40,8 @@ class BeeMetaInfo:
         Returns:
             :bool: True if bee has hatched before the given timestamp
         """
+        self._check_date(timestamp)
+
         bee_hatchdate = self.get_hatchdate(bee_id)
         return timestamp >= bee_hatchdate
 
@@ -49,5 +55,7 @@ class BeeMetaInfo:
         Returns:
             :class:`datetime.timedelta` Age of the bee at the given timestamp
         """
+        self._check_date(timestamp)
+
         bee_hatchdate = self.get_hatchdate(bee_id)
         return timestamp - bee_hatchdate
